aa20eb6a5a3a3f305f1ed5b071a7b09c24989d45
icesplendent <icesplendent@gmail.com>
2022-11-08 05:23:36 +0800

    feat:initial code

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include "accelerometer.h"
+#include "gyro.h"
+#include "mbed.h"
+
+AnalogOut Aout(PA_4);
+InterruptIn btnRecord(BUTTON1);
+EventQueue queue(32 * EVENTS_EVENT_SIZE);
+Thread t;
+Accelerometer acc;
+Gyro gyro;
+double Accel[3] = {0};
+double Gyro[3] = {0};
+double accAngleX = 0;
+double accAngleY = 0;
+double elapsedTime = 0;
+double roll, pitch, yaw;
+double gyroAngleX = 0;
+double gyroAngleY = 0;
+int counter = 0;
+int idR[32] = {0};
+int indexR = 0;
+double temp[3] = {0};
+
+void record(void) {
+    temp[0] = 0;
+    temp[1] = 0;
+    temp[2] = 0;
+
+    // 10 samples to average
+    for (int i = 0; i < 10; i++) {
+        acc.GetAcceleromterSensor(Accel);
+        acc.GetAcceleromterCalibratedData(Accel);
+        temp[0] += Accel[0];
+        temp[1] += Accel[1];
+        temp[2] += Accel[2];
+    }
+
+    Accel[0] = temp[0] / 10;
+    Accel[1] = temp[1] / 10;
+    Accel[2] = temp[2] / 10;
+
+    // printf("Calibrated ACC= %f, %f, %f\n", Accel[0], Accel[1], Accel[2]);
+
+    // Calculating Roll and Pitch from the accelerometer data
+    accAngleX =
+        (atan(Accel[1] / sqrt(Accel[0] * Accel[1] + Accel[2] * Accel[2])) *
+         180 / SENSOR_PI_DOUBLE);
+    accAngleY =
+        (atan(-1 * Accel[1] / sqrt(Accel[1] * Accel[1] + Accel[2] * Accel[2])) *
+         180 / SENSOR_PI_DOUBLE);
+
+    gyro.GetGyroSensor(Gyro);
+    gyro.GetGyroCalibratedData(Gyro);
+
+    // printf("Calibrated Gyro= %f, %f, %f\n", Gyro[0], Gyro[1], Gyro[2]);
+    elapsedTime = 0.1;  // 100ms by thread sleep time
+    // Currently the raw values are in degrees per seconds, deg/s, so we need to
+    // multiply by sendonds (s) to get the angle in degrees
+    gyroAngleX = gyroAngleX + Gyro[0] * elapsedTime;  // deg/s * s = deg
+    gyroAngleY = gyroAngleY + Gyro[1] * elapsedTime;
+    yaw = yaw + Gyro[2] * elapsedTime;
+    // Complementary filter - combine acceleromter and gyro angle values
+    // roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
+    // pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
+    // Use Acc data only
+    roll = accAngleX;
+    pitch = accAngleY;
+    printf("%f/%f/%f\n", roll, pitch, yaw);
+
+    // ThisThread::sleep_for(10ms);
+}
+
+// modify: record every 10ms, and average the results at 100ms (10 samples)
+void startRecord(void) {
+    // printf("---start---\n");
+    idR[indexR++] = queue.call_every(10ms, record);
+    indexR = indexR % 32;
+}
+
+void stopRecord(void) {
+    // printf("---stop---\n");
+    for (auto &i : idR) queue.cancel(i);
+}
+
+int main() {
+    t.start(callback(&queue, &EventQueue::dispatch_forever));
+    btnRecord.fall(queue.event(startRecord));
+    btnRecord.rise(queue.event(stopRecord));
+}
